b514af08be9ad180620a04908db5bd3426f32ecb
Villummmm <villumnejsum@gmail.com>
2025-01-20 13:36:03 +0000

    ninja

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let Danmark
+let DanmarkskortImg
+let Temperatur
+
+function preload(){
+  DanmarkskortImg = loadImage("");
+}
+
+
 function setup() {
-  createCanvas(2000, 1000);
+  createCanvas(200, 100);
 }
 
 function draw() {
-  background(220);
+  background(22);
+  rect(40,40,40)
 }
